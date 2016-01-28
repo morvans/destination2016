d091febb84413328ff9068d68a47f2bb02ee381a
Simon Morvan <garphy@zone84.net>
2016-01-28 22:34:50 +0100

    Visual return

--- app/main/MainController.js
+++ app/main/MainController.js
@@ -1,35 +1,50 @@
 'use strict';
 
-
 angular.module('jackpot.main')
-    .controller('MainController', MainController);
+  .controller('MainController', MainController);
 
-MainController.$inject = ['$rootScope'];
+MainController.$inject = ['$rootScope', '$scope'];
 
-function MainController($rootScope) {
-
+function MainController($rootScope, $scope) {
 
   var vm = this;
 
   activate();
 
-  function activate(){
+  function activate() {
 
-    vm.attemptStatus = null;
+    vm.showGreeting = false;
+    vm.attemptStatus = 'pending';
 
-    $rootScope.$on('attemptFailed', function(){
+    $rootScope.$on('attemptFailed', function () {
       vm.attemptStatus = 'failure';
+      $scope.$digest();
     });
-    $rootScope.$on('attemptSucceeded', function(){
+    $rootScope.$on('attemptSucceeded', function () {
+      vm.showGreeting = true;
       vm.attemptStatus = 'success';
+      $scope.$digest();
     });
 
   }
 
-  vm.random = function random(){
-    vm.attemptStatus = null;
+  vm.random = function random() {
+    vm.attemptStatus = 'pending';
     $rootScope.$broadcast('randomClicked');
   };
 
+  Object.defineProperty(vm, 'btnLabel', {
+    'get': function () {
+      switch (vm.attemptStatus) {
+        case 'success':
+          return 'Bravo ! Encore une fois ?';
+          break;
+        case 'failure':
+          return 'Essayez encore !';
+        default:
+          return '...';
+      }
+    }
+  })
 
 }
